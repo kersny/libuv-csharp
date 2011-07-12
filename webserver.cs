f1506e361c50e5104cad2d37c474076fbd584c1c
Kerry Snyder <kerry.snyder@gmail.com>
2011-07-11 23:25:15 -0400

    remove unused stuff

--- webserver.cs
+++ webserver.cs
@@ -42,37 +42,10 @@ namespace webserver {
 		[DllImport ("uvwrap")]
 		public static extern void uv_tcp_listen(IntPtr socket, int backlog, uv_connection_cb callback);
 		[DllImport ("uvwrap")]
-		public static extern sockaddr_in uv_ip4_addr(string host, int port);
-		[DllImport ("uvwrap")]
 		public static extern void uv_accept(IntPtr socket, IntPtr stream);
 		[DllImport ("uvwrap")]
 		public static extern int manos_uv_read_start(IntPtr stream, manos_uv_read_cb cb);
 		[DllImport ("uvwrap")]
 		public static extern int manos_uv_tcp_t_size();
-
-		//Stolen from http://www.elitepvpers.com/forum/co2-programming/159327-advanced-winsock-c.html
-		[StructLayout(LayoutKind.Sequential, Size=16)]
-		public struct sockaddr_in
-		{
-			public const int Size = 16;
-
-			public short sin_family;
-			public ushort sin_port;
-			public struct in_addr
-			{
-				public uint S_addr;
-				public struct _S_un_b
-				{
-					public byte s_b1, s_b2, s_b3, s_b4;
-				}
-				public _S_un_b S_un_b;
-				public struct _S_un_w
-				{
-					public ushort s_w1, s_w2;
-				}
-				public _S_un_w S_un_w;
-			}
-			public in_addr sin_addr;
-		}
 	}
 }
