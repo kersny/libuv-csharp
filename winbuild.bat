3e2a690e6c5475ae192cf930f95d1d4530eeb6c2
Kerry Snyder <kerry.snyder@gmail.com>
2011-07-13 11:58:38 -0400

    update win build

--- winbuild.bat
+++ winbuild.bat
@@ -9,7 +9,7 @@ IF %1==clean (
 	for /f %%b IN ('msbuildpath.exe') do %%bMSBuild.exe libuv\msvs\libuv.sln
 	call "%VS100COMNTOOLS%vsvars32.bat"
 	FOR /F %%a IN ('msbuildpath.exe') DO %%aMSBuild.exe webserver.csproj
-	"%VCINSTALLDIR%bin\cl.exe" uv_wrap.c /o build/uvwrap.dll libuv/build/Win32/Debug/libuv.lib ws2_32.lib advapi32.lib /link /DLL /EXPORT:uv_init /EXPORT:manos_uv_tcp_t_size /EXPORT:uv_tcp_init /EXPORT:uv_tcp_listen /EXPORT:uv_accept /EXPORT:manos_uv_read_start /EXPORT:uv_run /EXPORT:manos_uv_tcp_bind
+	"%VCINSTALLDIR%bin\cl.exe" uv_wrap.c /o build/uvwrap.dll libuv/build/Win32/Debug/libuv.lib ws2_32.lib advapi32.lib /link /DLL /EXPORT:uv_init /EXPORT:uv_tcp_init /EXPORT:uv_tcp_listen /EXPORT:uv_accept /EXPORT:manos_uv_read_start /EXPORT:uv_run /EXPORT:manos_uv_tcp_bind /EXPORT:manos_uv_tcp_t_create /EXPORT:manos_uv_tcp_t_destroy /EXPORT:manos_uv_write
 GOTO :EOF
 :Clean
 	rmdir /S /Q build
