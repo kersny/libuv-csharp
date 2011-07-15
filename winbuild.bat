9dbe5d5b040ec8d5ac75d42289c30ae6dadb7b45
Kerry Snyder <kerry.snyder@gmail.com>
2011-07-15 12:40:44 -0400

    use some batch file magic to do exports automatically

--- winbuild.bat
+++ winbuild.bat
@@ -6,13 +6,25 @@ IF %1==clean (
 	GOTO Build
 )
 :Build
+	del uv_wrap.def
+	echo LIBRARY UVWRAP >> uv_wrap.def
+	echo EXPORTS >> uv_wrap.def
+	set count=0
+	setlocal ENABLEDELAYEDEXPANSION
+	for /f "tokens=1,2,3,4,5 delims=( " %%a in ('findstr "extern" webserver.cs') do (
+		set /a count=count+1
+		echo %%e @!count! >> uv_wrap.def
+	)
 	for /f %%b IN ('msbuildpath.exe') do %%bMSBuild.exe libuv\msvs\libuv.sln
 	call "%VS100COMNTOOLS%vsvars32.bat"
 	FOR /F %%a IN ('msbuildpath.exe') DO %%aMSBuild.exe webserver.csproj
-	"%VCINSTALLDIR%bin\cl.exe" uv_wrap.c /o build/uvwrap.dll libuv/build/Win32/Debug/libuv.lib ws2_32.lib advapi32.lib /link /DLL /EXPORT:uv_init /EXPORT:uv_tcp_init /EXPORT:uv_tcp_listen /EXPORT:uv_accept /EXPORT:manos_uv_read_start /EXPORT:uv_run /EXPORT:manos_uv_tcp_bind /EXPORT:manos_uv_tcp_t_create /EXPORT:manos_uv_tcp_t_destroy /EXPORT:manos_uv_write
-GOTO :EOF
+	"%VCINSTALLDIR%bin\cl.exe" uv_wrap.c /o build/uvwrap.dll libuv/build/Win32/Debug/libuv.lib ws2_32.lib advapi32.lib /link /DLL /DEF:uv_wrap.def
+GOTO Done
 :Clean
 	rmdir /S /Q build
 	rmdir /S /Q libuv\build
 	del /Q uv_wrap.obj
-GOTO :EOF
+	del /Q uv_wrap.def
+GOTO Done
+:Done
+echo on
